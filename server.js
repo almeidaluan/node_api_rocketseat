d5473e8147f9cb0ac5988b24ab9ca092c7bb9dc5
almeidaluan <almeida.luan099@gmail.com>
2019-04-09 15:41:44 -0300

    Configuração do Cors

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const requireDir = require('require-dir')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 require('./src/db/db')
 
 requireDir('./src/model')
 
 
-app.use('/api',require('./src/routes'))
+app.use('/api', require('./src/routes'))
 
 app.listen(3000)
